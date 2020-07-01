5be114ce8e52daa0c898ef12c0a9613aba0fdbb0
netanel <netanel0205@gmail.com>
2020-07-02 00:26:17 +0300

    dns

---
+++ check_ip.py
@@ -0,0 +1,15 @@
+while True:
+    a = (input("enter ip : "))
+    s = a.split(".")
+    print(s)
+    if len(s) == 4:
+        for i in range(4):
+            if '1' > s[i] > '254':
+                print("no good")
+                break
+            else:
+                continue
+        print("the ip " + str(a) + " is good")
+    else:
+        print("no good")
+
